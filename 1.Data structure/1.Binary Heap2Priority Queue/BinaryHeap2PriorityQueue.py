a2f311c7468dd776b7d4ac73042bcf73de956639
chu <cx840212863@foxmail.com>
2020-04-13 18:15:32 +0800

    Binary Heap

--- 1.Data structure/1.Binary Heap2Priority Queue/BinaryHeap2PriorityQueue.py
+++ 1.Data structure/1.Binary Heap2Priority Queue/BinaryHeap2PriorityQueue.py
@@ -1,11 +1,10 @@
 class MaxPriorityQueue():
-    def __init__(self, cap):
-        self.mpq = []
-        self.N = 0
+    def __init__(self):
+        self.mpq = [0]
 
     @staticmethod
     def parent(root: int):
-        return root / 2
+        return int(root / 2)
 
     @staticmethod
     def left(root: int):
@@ -20,19 +19,56 @@ class MaxPriorityQueue():
             return self.mpq[1]
 
     def insert(self, element):
-        pass
+        self.mpq.append(element)
+        self.swim(len(self.mpq) - 1)
 
-    def del_max(self):
-        pass
+    def pop(self):
+        self.exchange(1, len(self.mpq) - 1)
+        top = self.mpq.pop()
+        self.sink(1)
+        return top
 
     def swim(self, k: int):
-        pass
+        while self.less(self.parent(k), k) and k > 1:
+            self.exchange(k, self.parent(k))
+            k = self.parent(k)
 
     def sink(self, k: int):
-        pass
+        while True:
+            print(self.mpq)
+            children = []
+            if self.left(k) < len(self.mpq) and self.less(k, self.left(k)):
+                children.append(self.left(k))
+            if self.right(k) < len(self.mpq) and self.less(k, self.right(k)):
+                children.append(self.right(k))
+            print("k =", k)
+            print("children =", children)
+            if not children:
+                break
+            index = self.max_index(children)
+            self.exchange(k, index)
+            k = index
 
     def exchange(self, a: int, b: int):
-        pass
+        self.mpq[a], self.mpq[b] = self.mpq[b], self.mpq[a]
 
-    def less(self, a: int, b: int):
+    def less(self, a: int, b: int) -> bool:
         return self.mpq[a] < self.mpq[b]
+
+    def max_index(self, children):
+        if len(children) == 1:
+            return children[-1]
+        else:
+            if self.mpq[children[0]] > self.mpq[children[1]]:
+                return children[0]
+            else:
+                return children[1]
+
+
+max_pq = MaxPriorityQueue()
+for i in range(1, 10):
+    max_pq.insert(i)
+    print(max_pq.mpq)
+print("###")
+print(max_pq.pop())
+print(max_pq.mpq)
